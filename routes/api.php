8ebf7008426e6e19bda4ddaa4b5ae35386b00de1
ADDFa <fratamaadha18@gmail.com>
2023-06-21 08:49:53 +0700

    register

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post("/login", "login");
     Route::post("/refresh", "refreshToken");
+    Route::post("/register", "register");
 });
 
 Route::controller(ResultController::class)->group(function () {
